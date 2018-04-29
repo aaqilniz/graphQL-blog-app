08a19fa3798ac97e9e2c36b14c7936a59622ed35
muhammad aaqil <aaqilcs102@gmail.com>
2018-04-30 01:01:21 +0500

    refactoring

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api');
+mongoose
+  .connect("mongodb://localhost:27017/api")
+  .then(success => {
+    console.log("mongoDB connection establishd successfully.");
+  })
+  .catch(err => {
+    console.log("failed to connect to mongoDB server.");
+  });
 
 module.exports = mongoose;
--- graphqlServer.js
+++ graphqlServer.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const basicAPI = require('./api/basicAPI');
-const userAPI = require('./api/userAPI');
+const basicAPI = require('./api/basic');
+const blogAPI = require('./api/blog');
 
 let app = express();
 
@@ -9,7 +9,7 @@ let app = express();
 app.use('/graphql/basic', basicAPI);
 
 //User API
-app.use('/graphql/user', userAPI);
+app.use('/graphql/user', blogAPI);
 
 app.listen(3000, () => {
     console.log('graphql server is up and running.');
